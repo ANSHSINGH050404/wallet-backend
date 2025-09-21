af1d4bdd923af9716e535f682b05d7c9f3e41c6f
ANSHSINGH050404 <114014895+ANSHSINGH050404@users.noreply.github.com>
2025-09-21 22:17:46 +0530

    add

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === "production") job.start();
 // middleware
 app.use(morgan("dev"));
 app.use(cors({
-  origin: ["http://localhost:8081",], // allow local + prod frontend
+  origin: ["*",], 
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true
 }));
