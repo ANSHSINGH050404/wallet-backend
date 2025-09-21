02fb9d5116521a5369e6d83fc0517c6f742f8e0d
ANSHSINGH050404 <114014895+ANSHSINGH050404@users.noreply.github.com>
2025-09-21 22:29:11 +0530

    remove node_env

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 const app = express();
 
-if (process.env.NODE_ENV === "production") job.start();
+// if (process.env.NODE_ENV === "production") job.start();
 
 // middleware
 app.use(morgan("dev"));
